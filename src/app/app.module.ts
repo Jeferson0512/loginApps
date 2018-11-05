e9686fc95f4322e8e01b2f674d18a8fe581cca8b
Alumno <Alumno@LAB402-23>
2018-11-05 17:48:38 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { PrivatePageComponent } from './components/private-page/private-page.component';
+import { NotfoundPageComponent } from './components/notfound-page/notfound-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    PrivatePageComponent,
+    NotfoundPageComponent
   ],
   imports: [
     BrowserModule,
