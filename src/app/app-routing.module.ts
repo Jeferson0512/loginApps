7fe7d9d01fb4cea28152a80c27ed3dfc5d93451b
Jeferson0512 <jeferson.bujaico@tecsup.edu.pe>
2018-11-06 19:59:20 -0500

    Se hizo todo el navbar y las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { PrivatePageComponent } from './components/private-page/private-page.component';
+import { NotfoundPageComponent } from './components/notfound-page/notfound-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'registrar', component: RegisterPageComponent},
+  {path: 'privado', component: PrivatePageComponent},
+  {path: '**', component: NotfoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
